e1f39d37a98b9eca69f63715b60cdf925e65bd5c
Sondre Nilsen <sondre.nilsen@nav.no>
2021-08-05 09:42:20 +0200

    Remove unused code
    Co-authored-by: Erlend <erlend.marius.ommundsen@nav.no>
    Co-authored-by: Sondre Halvorsen <sondre.halvorsen@nav.no>

--- pkg/service/app.go
+++ pkg/service/app.go
@@ -1,16 +1,10 @@
 package service
 
 import (
-	"context"
-	"sort"
-
 	"github.com/Unleash/unleash-client-go/v3"
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	"github.com/nais/babylon/pkg/config"
 	"github.com/nais/babylon/pkg/metrics"
-	nais_io_v1 "github.com/nais/liberator/pkg/apis/nais.io/v1"
-	log "github.com/sirupsen/logrus"
-	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -20,75 +14,4 @@ type Service struct {
 	Metrics       *metrics.Metrics
 	UnleashClient *unleash.Client
 	InfluxClient  influxdb2.Client
-}
-
-const defaultChannel = "#babylon-alerts"
-
-func (s *Service) getUnleash(name string) bool {
-	if s.UnleashClient == nil {
-		log.Info("Unleashed client not configured, defaulting to false")
-
-		return false
-	}
-
-	return s.UnleashClient.IsEnabled(name)
-}
-
-func (s *Service) SlackChannel(ctx context.Context, ns string) string {
-	if !s.getUnleash("babylon_alerts") {
-		return defaultChannel
-	}
-
-	namespace := &v1.Namespace{}
-	key := client.ObjectKey{Name: ns}
-	err := s.Client.Get(ctx, key, namespace)
-	if err != nil {
-		log.Errorf("Failed to get namespace %v, got error %v", ns, err)
-
-		return defaultChannel
-	}
-
-	ch, ok := namespace.Annotations["platform-alerts-channel"]
-	if ok {
-		return ch
-	}
-
-	ch, ok = s.existingAlertChannel(ctx, ns)
-	if ok {
-		return ch
-	}
-
-	ch, ok = namespace.Annotations["slack-channel"]
-	if ok {
-		return ch
-	}
-
-	log.Warnf("Namespace %s does not have a slack-channel-annotation", ns)
-
-	return defaultChannel
-}
-
-// Get an existing alert channel in use by looking at NAIS alerts.
-func (s *Service) existingAlertChannel(ctx context.Context, ns string) (string, bool) {
-	alerts := &nais_io_v1.AlertList{}
-	err := s.Client.List(ctx, alerts, &client.ListOptions{Namespace: ns})
-	if err != nil {
-		log.Errorf("Failed to list alerts in namespace %s, got error %v", ns, err)
-
-		return "", false
-	}
-	// Sort alerts to avoid random channel picks
-	sort.Slice(alerts.Items, func(i, j int) bool {
-		return alerts.Items[i].Spec.Receivers.Slack.Channel < alerts.Items[j].Spec.Receivers.Slack.Channel
-	})
-	for _, alert := range alerts.Items {
-		ch := alert.Spec.Receivers.Slack.Channel
-		if ch == "" {
-			continue
-		}
-
-		return ch, true
-	}
-
-	return "", false
 }
