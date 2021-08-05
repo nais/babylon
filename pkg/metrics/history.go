a88496805163d067b54330fe9fb3e0130ad6a15b
Sondre Halvorsen <Sondre.Halvorsen@nav.no>
2021-08-05 14:12:07 +0200

    Use tags rather than fields in InfluxDB to make values queryable
    Co-authored-by: Sondre Nilsen <sondre.nilsen@nav.no>
    Co-authored-by: Erlend <Erlend.Marius.Ommundsen@nav.no>

--- pkg/metrics/history.go
+++ pkg/metrics/history.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
@@ -17,9 +18,9 @@ func NewHistory(influxClient influxdb2.Client, influxdbDatabase string) *History
 	return &History{influxClient: influxClient, InfluxdbDatabase: influxdbDatabase}
 }
 
-func (h *History) historize(measurement string, data map[string]interface{}) {
+func (h *History) historize(measurement string, data map[string]string) {
 	writeAPI := h.influxClient.WriteAPIBlocking("", h.InfluxdbDatabase+"/autogen")
-	p := influxdb2.NewPoint(measurement, map[string]string{}, data, time.Now())
+	p := influxdb2.NewPoint(measurement, data, map[string]interface{}{}, time.Now())
 
 	err := writeAPI.WritePoint(context.Background(), p)
 	if err != nil {
@@ -28,13 +29,13 @@ func (h *History) historize(measurement string, data map[string]interface{}) {
 }
 
 func (h *History) HistorizeDeploymentFailing(reason, team, slackChannel, name string) {
-	go h.historize("deployment_failing", map[string]interface{}{
+	go h.historize("deployment_failing", map[string]string{
 		"reason": reason, "team": team, "slack_channel": slackChannel, "name": name,
 	})
 }
 
 func (h *History) HistorizeDeploymentKilled(method, team, slackChannel, name string, armed bool) {
-	go h.historize("deployment_killed", map[string]interface{}{
-		"method": method, "team": team, "slack_channel": slackChannel, "name": name, "dry_run": !armed,
+	go h.historize("deployment_killed", map[string]string{
+		"method": method, "team": team, "slack_channel": slackChannel, "name": name, "dry_run": fmt.Sprint(!armed),
 	})
 }
